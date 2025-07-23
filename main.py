49b1f7890c8e9a9fdfc93d1f4091489c1268fdb5
Faiz Ghifari Haznitrama <fgh1997@live.com>
2025-07-23 06:05:54 +0000

    adjust new swm pipeline

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from huggingface_hub import login
-import transformers
 import torch
 import numpy as np
 from tqdm.auto import tqdm
@@ -9,6 +7,7 @@ import string
 
 from model_wrapper import ModelWrapper
 from swm import image_swm
+
 
 def run_swm(model, n_boxes, n_tokens=1, cot=None, think_budget=64, note_assist=False):
     """
@@ -45,9 +44,9 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
     tokens = [string.ascii_uppercase[x] for x in range(n_tokens)]
     legal_boxes = dict.fromkeys(tokens)
     for token in tokens:
-        legal_boxes[token] = [x for x in range(1, n_boxes+1)]
+        legal_boxes[token] = [x for x in range(1, n_boxes + 1)]
 
-    worst_case_n = n_boxes ** 2
+    worst_case_n = n_boxes**2
     total_guess = 0
     illegal_guess = 0
     invalid_guess = 0
@@ -63,7 +62,7 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
                 # tqdm.write(f"Token {token} put in box {token_box[token]}")
             found_tokens = []
 
-            while (True):
+            while True:
                 for token in found_tokens:
                     if len(legal_boxes[token]) == 0:
                         continue
@@ -73,13 +72,13 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
                 # Save to temp file
                 with open("data/temp_history.json", "w") as f:
                     json.dump(model.history, f, indent=4)
-                
+
                 # End test
                 if all([len(legal) == 0 for legal in legal_boxes.values()]):
                     break
                 if total_guess >= worst_case_n:
                     break
-                
+
                 opened_boxes = set()
                 found_tokens = []
                 found = False
@@ -89,16 +88,16 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
 
                     with open("data/temp_history.json", "w") as f:
                         json.dump(model.history, f, indent=4)
-                    
+
                     if total_guess >= worst_case_n:
                         break
-                    
+
                     # Note-taking assistance
+                    notes = ""
                     if note_assist:
-                        notes = ""
                         for token, legal in legal_boxes.items():
                             notes += f"Boxes that has contained token {token}: "
-                            for box in range(1, n_boxes+1):
+                            for box in range(1, n_boxes + 1):
                                 if box not in legal:
                                     notes += f"{box}, "
                             notes += "\n"
@@ -106,26 +105,40 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
                         for box in opened_boxes:
                             notes += f"{box}, "
                         notes += "\n"
-                    
+
                     msg = ""
                     for token in tokens:
                         msg += f"{token} tokens found: {n_boxes - len(legal_boxes[token])}\n"
 
                     # Get and validate response
-                    if re.search(r"<answer>(.*?)</answer>", response) is not None:
-                        chosen_box = re.search(r"<answer>(.*?)</answer>", response)[0]
-                        chosen_box = re.sub(r"<answer>|</answer>", "", chosen_box).strip()
+                    match = re.search(r"<answer>\s*([\s\S]*?)\s*</answer>", response)
+                    if match is not None:
+                        chosen_box = match.group(1).strip()
                         try:
                             chosen_box = int(chosen_box)
                         except ValueError:
-                            response = model.send_message(f"Please answer with a box number (1-{n_boxes}).\n" + msg + notes + question, truncate_history=True, cot=cot)
+                            response = model.send_message(
+                                f"Please answer with a box number (1-{n_boxes}).\n"
+                                + msg
+                                + notes
+                                + question,
+                                truncate_history=True,
+                                cot=cot,
+                            )
                             invalid_guess += 1
                             continue
                     else:
-                        response = model.send_message(f"Please answer with the specified format\n" + msg + notes + question, truncate_history=True, cot=cot)
+                        response = model.send_message(
+                            f"Please answer with the specified format\n"
+                            + msg
+                            + notes
+                            + question,
+                            truncate_history=True,
+                            cot=cot,
+                        )
                         invalid_guess += 1
                         continue
-                    
+
                     legal = False
                     for legal in legal_boxes.values():
                         if chosen_box in legal:
@@ -145,7 +158,7 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
                             token_bar.update(1)
                             legal_boxes[token].remove(chosen_box)
                             found_tokens.append(token)
-                    
+
                     msg = ""
                     if found:
                         for token in found_tokens:
@@ -153,8 +166,10 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
                     else:
                         msg += f"No tokens found in box {chosen_box}.\n" + msg
 
-                    response = model.send_message(msg + notes + question, truncate_history=True, cot=cot)
-                    model.history[-2]["content"] = msg      # Truncate user response length
+                    response = model.send_message(
+                        msg + notes + question, truncate_history=True, cot=cot
+                    )
+                    model.history[-2]["content"] = msg  # Truncate user response length
 
     run_stats = {
         "worst_case_guesses": worst_case_n,
@@ -166,6 +181,7 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
 
     return run_stats
 
+
 def score(run_stats):
     """
     Score the run statistics from the SWM test.
@@ -174,37 +190,66 @@ def score(run_stats):
     Returns:
         float: The score.
     """
-    return 1 - (run_stats['illegal'] + run_stats['repeated']) / (run_stats['guesses'] - run_stats['invalid'])
+    return 1 - (run_stats["illegal"] + run_stats["repeated"]) / (
+        run_stats["guesses"] - run_stats["invalid"]
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the SWM problem.")
     parser.add_argument("--model", type=str, default=None, help="The model to use.")
-    parser.add_argument("--model_source", type=str, default="hf", help="The source of the model.")
-    parser.add_argument("--n_boxes", type=int, default=6, help="The number of boxes in the test. More == harder.")
-    parser.add_argument("--n_tokens", type=int, default=1, help="The number of different tokens present at the same time. More == harder")
+    parser.add_argument(
+        "--model_source", type=str, default="openai", help="The source of the model."
+    )
+    parser.add_argument(
+        "--n_boxes",
+        type=int,
+        default=6,
+        help="The number of boxes in the test. More == harder.",
+    )
+    parser.add_argument(
+        "--n_tokens",
+        type=int,
+        default=1,
+        help="The number of different tokens present at the same time. More == harder",
+    )
     parser.add_argument("--cot", action="store_true")
-    parser.add_argument("--runs", type=int, default=1, help="The number of runs to perform.")
-    parser.add_argument("--max_tokens", type=int, default=512, help="The maximum number of tokens to generate.")
-    parser.add_argument("--think_budget", type=int, default=64, help="The budget tokens for reasoning.")
+    parser.add_argument(
+        "--runs", type=int, default=1, help="The number of runs to perform."
+    )
+    parser.add_argument(
+        "--max_tokens",
+        type=int,
+        default=512,
+        help="The maximum number of tokens to generate.",
+    )
+    parser.add_argument(
+        "--think_budget", type=int, default=64, help="The budget tokens for reasoning."
+    )
     parser.add_argument("--notes", action="store_true")
     parser.add_argument("--image", action="store_true")
-    parser.add_argument("--api_key", type=str, default=None, help="API key to use. If none, uses key stored in environment variable.")
+    parser.add_argument(
+        "--api_key",
+        type=str,
+        default=None,
+        help="API key to use. If none, uses key stored in environment variable.",
+    )
     args = parser.parse_args()
 
     # Input validation
-    if args.model_source not in ["hf", "google", "litellm", "vllm"]:
-        raise ValueError("Model source must be either 'hf', 'google', 'litellm', or 'vllm'.")
+    if args.model_source not in ["vllm", "openai", "openrouter"]:
+        raise ValueError(
+            "Model source must be either 'vllm', 'openai', or 'openrouter'."
+        )
 
     if args.model is None:
-        if args.model_source == "hf":
-            args.model = "unsloth/Meta-Llama-3.1-8B-Instruct"
-        elif args.model_source == "google":
-            args.model = "gemini-1.5-flash-8b"
-        elif args.model_source == "litellm":
-            args.model = "gpt-4o-mini-2024-07-18"
-        elif args.model_source == "vllm":
-            args.model = "meta-llama/Llama-2-7b-chat-hf"
-    
+        if args.model_source == "vllm":
+            args.model = "Qwen/Qwen3-32B"
+        elif args.model_source == "openai":
+            args.model = "o4-mini-2025-04-16"
+        elif args.model_source == "openrouter":
+            args.model = "qwen/qwen3-235b-a22b-07-25"
+
     if args.image:
         if args.notes:
             os.makedirs("./data/image/baselines", exist_ok=True)
@@ -218,7 +263,6 @@ if __name__ == "__main__":
         else:
             os.makedirs("./data/text", exist_ok=True)
         img_path = None
-    
 
     file_header = f"data/{'image/' if args.image else 'text/'}{'baselines/' if args.notes else ''}{args.model_source}_{args.model.replace('/', '-')}{'_cot' if args.cot else ''}_{args.n_boxes}_{args.n_tokens}_"
     print(f"Saving to: {file_header}")
@@ -226,63 +270,86 @@ if __name__ == "__main__":
     # Check if results already exist
     stats_file = file_header + "run_stats.json"
     history_file = file_header + "run_history.json"
-    
+
     if os.path.exists(stats_file) and os.path.exists(history_file):
         print(f"Results already exist at {stats_file}")
-        with open(stats_file, 'r') as f:
+        with open(stats_file, "r") as f:
             run_stats = json.load(f)
-            
+
         # Calculate and display statistics
         avg_stats = {}
         for key in run_stats["run_1"].keys():
             if type(run_stats["run_1"][key]) == int:
                 avg_stats[key] = np.mean([stats[key] for stats in run_stats.values()])
-        
+
         for key, value in avg_stats.items():
             print(f"{key}: {value}")
-        
+
         tot_score = 0
         for stats in run_stats.values():
             tot_score += score(stats)
-        print(f'Score: {tot_score / len(run_stats.keys())}')
+        print(f"Score: {tot_score / len(run_stats.keys())}")
         exit(0)
 
     run_stats = {}
     run_history = {}
     run_reasoning = {}  # Add new dictionary for reasoning traces
-    
+
     for i in range(args.runs):
         model = None
         torch.cuda.empty_cache()
-        
-        model = ModelWrapper(args.model, args.model_source, api_key=args.api_key, max_new_tokens=args.max_tokens, image_input=args.image, image_path=img_path)
+
+        model = ModelWrapper(
+            args.model,
+            args.model_source,
+            api_key=args.api_key,
+            max_new_tokens=args.max_tokens,
+            image_input=args.image,
+            image_path=img_path,
+        )
 
         print(f"Run {i+1}/{args.runs}")
         if args.image:
-            run_stats[f"run_{i+1}"] = image_swm(model, args.n_boxes, n_tokens=args.n_tokens, cot=args.cot, think_budget=args.think_budget, note_assist=args.notes)
+            run_stats[f"run_{i+1}"] = image_swm(
+                model,
+                args.n_boxes,
+                n_tokens=args.n_tokens,
+                cot=args.cot,
+                think_budget=args.think_budget,
+                note_assist=args.notes,
+            )
         else:
-            run_stats[f"run_{i+1}"] = run_swm(model, args.n_boxes, n_tokens=args.n_tokens, cot=args.cot, think_budget=args.think_budget, note_assist=args.notes)
+            run_stats[f"run_{i+1}"] = run_swm(
+                model,
+                args.n_boxes,
+                n_tokens=args.n_tokens,
+                cot=args.cot,
+                think_budget=args.think_budget,
+                note_assist=args.notes,
+            )
         run_history[f"run_{i+1}"] = model.history
         run_reasoning[f"run_{i+1}"] = model.reasoning_trace  # Save reasoning trace
 
         with open(file_header + "run_stats.json", "w") as f:
             json.dump(run_stats, f, indent=4)
-        
+
         with open(file_header + "run_history.json", "w") as f:
             json.dump(run_history, f, indent=4)
-            
-        with open(file_header + "run_reasoning.json", "w") as f:  # Save reasoning traces
+
+        with open(
+            file_header + "run_reasoning.json", "w"
+        ) as f:  # Save reasoning traces
             json.dump(run_reasoning, f, indent=4)
 
     avg_stats = {}
     for key in run_stats["run_1"].keys():
         if type(run_stats["run_1"][key]) == int:
             avg_stats[key] = np.mean([stats[key] for stats in run_stats.values()])
-    
+
     for key, value in avg_stats.items():
         print(f"{key}: {value}")
-    
+
     tot_score = 0
     for stats in run_stats.values():
         tot_score += score(stats)
-    print(f'Score: {tot_score / len(run_stats.keys())}')
+    print(f"Score: {tot_score / len(run_stats.keys())}")
