a852475e2cb3aa69b5d152c24c7122894e52b4c5
reggans <faeyza.rishad@kaist.ac.kr>
2026-01-04 23:11:03 +0900

    test environment class added

--- SWM/__init__.py
+++ SWM/__init__.py
@@ -4,5 +4,7 @@ Spatial Working Memory (SWM) test module.
 
 from .main import swm_main, score
 from .swm import image_swm
+from .swm_env import SWMEnv
+from .image import SWMImage
 
-__all__ = ["swm_main", "score", "image_swm"]
+__all__ = ["swm_main", "score", "image_swm", "SWMEnv", "SWMImage"]
