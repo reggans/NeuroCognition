048c3415e9512c3c92c581da8d1462404131ed77
faizghifari <fgh1997@live.com>
2026-01-20 15:48:34 +0900

    fix swm reward design and rubric

--- SWM/__init__.py
+++ SWM/__init__.py
@@ -1,10 +1,19 @@
-"""
-Spatial Working Memory (SWM) test module.
-"""
+"""Spatial Working Memory (SWM) test module."""
 
 from .main import swm_main, score, run_swm_with_env
 from .swm import image_swm
-from .swm_env import SWMEnv
+from .swm_mt_env import load_environment, SWMMultiTurnEnv, create_swm_dataset
+from .swm_rubric import SWMRubric
 from .image import SWMImage
 
-__all__ = ["swm_main", "score", "run_swm_with_env", "image_swm", "SWMEnv", "SWMImage"]
+__all__ = [
+    "swm_main",
+    "score",
+    "run_swm_with_env",
+    "image_swm",
+    "load_environment",
+    "SWMMultiTurnEnv",
+    "create_swm_dataset",
+    "SWMRubric",
+    "SWMImage",
+]
